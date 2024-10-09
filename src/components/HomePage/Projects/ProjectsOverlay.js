ccbbb1652c131b15a585996ee127eb03d94ab6c1
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-09 07:49:29 +0100

    overlay

--- src/components/HomePage/Projects/ProjectsOverlay.js
+++ src/components/HomePage/Projects/ProjectsOverlay.js
@@ -5,35 +5,7 @@ export const LaonDetailsOverlay = ({
   return (
     <div
       onClick={openLaonDetailsHandler}
-      className={`fixed top-0 left-0 z-10 w-full h-screen bg-orange-50/50 transition-all duration-500 ${
-        openLaonDetails ? 'block' : 'hidden'
-      }`}
-    />
-  );
-};
-
-export const BettershotzDetailsOverlay = ({
-  openBettershotzDetails,
-  openBettershotzDetailsHandler,
-}) => {
-  return (
-    <div
-      onClick={openBettershotzDetailsHandler}
-      className={`fixed top-0 left-0 z-10 w-full h-screen bg-orange-50/50 transition-all duration-500 ${
-        openBettershotzDetails ? 'block' : 'hidden'
-      }`}
-    />
-  );
-};
-
-export const MfonAiDetailsOverlay = ({
-  openMfonAiDetails,
-  openMfonAiDetailsHandler,
-}) => {
-  return (
-    <div
-      onClick={openMfonAiDetailsHandler}
-      className={`fixed top-0 left-0 z-10 w-full h-screen bg-orange-50/50 transition-all duration-500 ${
+      claition-all duration-500 ${
         openMfonAiDetails ? 'block' : 'hidden'
       }`}
     />
